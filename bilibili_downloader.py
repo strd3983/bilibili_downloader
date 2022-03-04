211c751020792f8a38e40f3520320ae7cc95942f
strd3983 <strd3983@gmail.com>
2022-03-04 21:21:47 +0900

    v1.2 - Python 3.10に更新、一部誤字を修正

--- bilibili_downloader.py
+++ bilibili_downloader.py
@@ -5,7 +5,8 @@ import time
 
 headers = {
     'referer': 'https://www.bilibili.com',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)\
+        Chrome/91.0.4472.114 Safari/537.36'
 }
 
 quality_dict = {
@@ -29,32 +30,6 @@ quality_dict = {
 TIME = 5
 
 
-class Color:
-    BLACK = '\033[30m'  # (文字)黒
-    RED = '\033[31m'  # (文字)赤
-    GREEN = '\033[32m'  # (文字)緑
-    YELLOW = '\033[33m'  # (文字)黄
-    BLUE = '\033[34m'  # (文字)青
-    MAGENTA = '\033[35m'  # (文字)マゼンタ
-    CYAN = '\033[36m'  # (文字)シアン
-    WHITE = '\033[37m'  # (文字)白
-    COLOR_DEFAULT = '\033[39m'  # 文字色をデフォルトに戻す
-    BOLD = '\033[1m'  # 太字
-    UNDERLINE = '\033[4m'  # 下線
-    INVISIBLE = '\033[08m'  # 不可視
-    REVERCE = '\033[07m'  # 文字色と背景色を反転
-    BG_BLACK = '\033[40m'  # (背景)黒
-    BG_RED = '\033[41m'  # (背景)赤
-    BG_GREEN = '\033[42m'  # (背景)緑
-    BG_YELLOW = '\033[43m'  # (背景)黄
-    BG_BLUE = '\033[44m'  # (背景)青
-    BG_MAGENTA = '\033[45m'  # (背景)マゼンタ
-    BG_CYAN = '\033[46m'  # (背景)シアン
-    BG_WHITE = '\033[47m'  # (背景)白
-    BG_DEFAULT = '\033[49m'  # 背景色をデフォルトに戻す
-    RESET = '\033[0m'  # 全てリセット
-
-
 # --------------------------------------------------
 # メイン関数
 # --------------------------------------------------
@@ -62,7 +37,7 @@ def main():
     print('###############################')
     print('   ビリビリ動画ダウンローダ')
     print('###############################\n')
-    print('M: マイリスid(ml[数字])または動画id(BV[文字列])を入力\n>> ', end='')
+    print('M: マイリスid(ml[数字])または動画id(BV[文字列])を入力\n >> ', end='')
     ml_title, bvids = get_bvid(input())
     print('M: マイリストのタイトル:', ml_title)
     print('M: ダウンロードする動画数:', len(bvids))
